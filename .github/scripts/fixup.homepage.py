c1a039db62ee21b57e7f2ef538a75892039b426d
Bozo Dragojevic <bozzo@digiverse.si>
2021-12-13 16:38:28 +0100

    frendly PR deploy dirs

--- .github/scripts/fixup.homepage.py
+++ .github/scripts/fixup.homepage.py
@@ -4,7 +4,13 @@ import os
 with open("package.json") as f:
   package = json.load(f)
 
-package["homepage"] += os.environ["GITHUB_SHA"] + "/"
+destination = "/".join(p for p in os.environ["GITHUB_REF_NAME"].split('/') if p not in ('merge', 'ref'))
+if not destination.endswith("/"):
+  destination += "/"
+
+package["homepage"] += destination
 
 with open("package.json", "w") as f:
   json.dump(package, f, indent=2)
+
+print(destination)
